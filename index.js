1cb3775b4cabe904215bca00b891f25cae4b42d6
tatsuyalam <tatsuyalam@gmail.com>
2016-10-02 23:38:11 +0700

    Correct logging

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 let http = require('http')
 let request = require('request')
-let desUrl = '127.0.0.1:8080'
+// Set a the default value for --host to 127.0.0.1
+let url = require('url')
+let argv = require('yargs').default('host', '127.0.0.1').argv
+// Get the --port value
+let port = argv.port || (argv.host === '127.0.0.1' ? 8080 : 80)
+let desUrl = argv.url || url.format({
+   protocol: 'http',
+   hostname: argv.host,
+   port
+})
 
 http.createServer((req, res) => {
 
-  console.log(`Echo server received at: ${req.url}`)
+  console.log(`\n\nEcho server received at: ${req.url}`)
 
   // Basic server
   // res.end('Hello world from NodeStart!\n')
@@ -16,29 +25,29 @@ http.createServer((req, res) => {
   req.pipe(res)
 
   // Basic logging
-  // process.stdout.write('\n\n\n' + JSON.stringify(req.headers))
-  // req.pipe(process.stdout)
+  process.stdout.write(JSON.stringify(req.headers) + '\n')
+  req.pipe(process.stdout)
 
-}).listen(8080)
-console.log('Echo server: Listening on port 8080...')
+}).listen(port)
+console.log(`Echo server: Listening on port ${port}...`)
 
 http.createServer((req, res) => {
 
-  console.log(`Proxying request to: ${desUrl + req.url}`)
+  console.log(`\n\nProxying request to: ${desUrl + req.url}`)
 
   // Proxy server
   let options = {
     headers: req.headers,
-    url: `http://${desUrl}${req.url}`
+    url: `${desUrl}${req.url}`
   }
   options.method = req.method // use the same HTTP verb (for non-GET requests)
   let outRes = request(options)
-  req.pipe(outRes).pipe(res) // streams are chainable
+  req.pipe(outRes) // streams are chainable
 
   // Logging
-  // process.stdout.write(JSON.stringify(outRes.headers))
-  // outRes.pipe(process.stdout)
-  // outRes.pipe(res)
+  process.stdout.write(JSON.stringify(outRes.headers) + '\n')
+  outRes.pipe(process.stdout)
+  outRes.pipe(res)
 
 }).listen(8001)
 console.log('Proxy server: Listening on port 8001...')
@@ -56,3 +65,7 @@ console.log('Proxy server: Listening on port 8001...')
 // 1. Add request package : `$ npm install --save request`
 // 2. Update proxy code
 // 3. Test command:     `$ curl -v http://127.0.0.1:8001/org-uri -d "hello proxy"`
+
+// D - Configuration with a CLI
+// 1. Start test server:  `$ node index.js --port=8003`
+// 2. Proxy command:      `$ curl -v http://127.0.0.1:8001/org-uri -d "request with a CLI"`
